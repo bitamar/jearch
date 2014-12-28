10663e20672e8982258fc66529894becc103cd43
Itamar Shapira Bar-Lev <bitamar@gmail.com>
2014-12-28 10:37:43 +0200

    Adding search controller

--- app/js/search.js
+++ app/js/search.js
@@ -1,6 +1,55 @@
 'use strict';
 
-var app = angular.module('jekyllSearch', []).config(['$interpolateProvider', function($interpolateProvider){
+var app = angular.module('jekyllSearch', ['ngSanitize', 'ui.select']).config(['$interpolateProvider', function($interpolateProvider) {
   $interpolateProvider.startSymbol('{(').endSymbol(')}');
 }]);
 
+app.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.post = {};
+  $scope.searchTerms = [];
+  $scope.posts = [];
+
+  $http.get('words.json').success(function(data) {
+    $scope.searchTerms = data;
+  });
+  $http.get('posts.json').success(function(data) {
+    $scope.posts = data;
+  });
+}]);
+
+/**
+ * AngularJS default filter with the following expression:
+ * "person in people | filter: {name: $select.search, age: $select.search}"
+ * performs a AND between 'name: $select.search' and 'age: $select.search'.
+ * We want to perform a OR.
+ */
+app.filter('postsFilter', function() {
+  return function(items, props) {
+    var out = [];
+
+    if (angular.isArray(items)) {
+      items.forEach(function(item) {
+        var itemMatches = false;
+
+        var keys = Object.keys(props);
+        for (var i = 0; i < keys.length; i++) {
+          var prop = keys[i];
+          var text = props[prop].toLowerCase();
+          //if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+          //  itemMatches = true;
+          //  break;
+          //}
+        }
+
+        if (itemMatches) {
+          out.push(item);
+        }
+      });
+    } else {
+      // Let the output be the input untouched
+      out = items;
+    }
+
+    return out;
+  }
+});
