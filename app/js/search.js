8c16f69b78d90c487d30b718df93b60bd8c4867f
Itamar Shapira Bar-Lev <bitamar@gmail.com>
2015-01-06 09:41:48 +0200

    Starting to search partial words

--- app/js/search.js
+++ app/js/search.js
@@ -19,16 +19,27 @@ app.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
 
 app.filter('postsFilter', function() {
   return function(postsTitles, search) {
-
+    if (!search.phrase) {
+      return;
+    }
     var words = search.phrase.toLowerCase().split(' ');
 
     var postIds = _.range(postsTitles.length);
 
+    // Search for full word matches.
     words.forEach(function(word) {
       if (word.length > 2) {
         postIds = _.intersection(postIds, search.scope.searchTerms[word]);
       }
     });
+
+    // Search for partial word matches when there are few results.
+    if (postIds < 10) {
+      words.forEach(function(word) {
+        console.log(word);
+        //search.scope.searchTerms ;
+      });
+    }
 
     // Fetch posts according to the ids list.
     var posts = [];
